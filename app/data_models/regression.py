92b913e3d3738d8a507db9eaa353b93b05c03352
Beshoy Fawzy <besho_waheeb@yahoo.com>
2024-06-21 18:02:10 +0200

    added restrictions on input data

--- app/data_models/regression.py
+++ app/data_models/regression.py
@@ -1,17 +1,31 @@
-from pydantic import BaseModel, Field
-from typing import Optional, Annotated
+from datetime import datetime
+from pydantic import BaseModel, Field, validator
+from pydantic.types import Annotated, StringConstraints
 
 
 class RawCarDataRegression(BaseModel):
     product_tier: Annotated[str, Field(min_length=1)]
     make_name: Annotated[str, Field(min_length=1)]
-    price: int
-    first_zip_digit: int
+    price: Annotated[float, Field(gt=0)]
+    first_zip_digit: Annotated[int, Field(ge=0, le=9)]
     first_registration_year: int
-    stock_days: int
-    created_date: str
-    search_views: int
+    stock_days: Annotated[int, Field(gt=0)]
+    created_date: Annotated[str, StringConstraints(pattern=r"^\d{4}-\d{2}-\d{2}$")]
+    search_views: Annotated[int, Field(gt=0)]
     ctr: float
+
+    @validator("first_registration_year")
+    def year_range(cls, v):
+        current_year = datetime.now().year
+        min_year = current_year - 50  # 50 years ago, this is to be tuned as required
+        max_year = current_year  # Current year
+
+        if v < min_year or v > max_year:
+            raise ValueError(
+                f"first_registration_year must be between {min_year} and {max_year}"
+            )
+
+        return v
 
     class Config:
         arbitrary_types_allowed = True
